602ffea29a19cc2eeaa15ccdf866fed75dbdb5ba
Bhushan Malgaonkar <bmalgaon@iu.edu>
2018-12-18 10:09:43 +0530

    parameter estimation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import math
+from time import time
+
 from pos_data import read
 from models.simple import Simple
 from models.hmm import HMM
@@ -20,7 +23,30 @@ X_val, y_val = read('data/bc.val', 'train')
 # y_pred = hm.predict(X_val)
 # print_report(y_val, y_pred)
 
-cp = Complex()
-cp.fit(X_train, y_train)
-y_pred = cp.predict(X_val)
-print_report(y_val, y_pred)
+for missing_word_cost in [10e-6, 10e-9, 10e-11, 10e-13]:
+    for missing_emission_cost in [10e-6, 10e-9, 10e-11, 10e-13]:
+        for missing_transition_1_cost in [10e-6, 10e-9, 10e-11, 10e-13]:
+            for missing_transition_2_cost in [10e-6, 10e-9, 10e-11, 10e-13]:
+                for gibbs in [10, 20, 50, 100, 150]:
+                    st = time()
+
+                    cp = Complex()
+                    cp.MISSING_WORD_PROBABILITY = missing_word_cost
+                    cp.MISSING_WORD_COST = -math.log(missing_word_cost)
+                    cp.MISSING_EMISSION_COST = -math.log(missing_emission_cost)
+                    cp.MISSING_TRANSITION_1_COST = -math.log(missing_transition_1_cost)
+                    cp.MISSING_TRANSITION_2_COST = -math.log(missing_transition_2_cost)
+                    cp.NUM_GIBBS_ITER = gibbs
+
+                    cp.fit(X_train, y_train)
+                    y_pred = cp.predict(X_val)
+
+                    print('{:50s}: {}'.format('Missing word', missing_word_cost))
+                    print('{:50s}: {}'.format('Missing emission', missing_emission_cost))
+                    print('{:50s}: {}'.format('Missing transition 1', missing_transition_1_cost))
+                    print('{:50s}: {}'.format('Missing transition 2', missing_transition_2_cost))
+                    print('{:50s}: {}'.format('Gibbs samples', gibbs))
+
+                    print_report(y_val, y_pred)
+                    print('{:50s}: {}'.format('Time taken', int(time() - st)))
+                    print()
